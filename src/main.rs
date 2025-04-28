972edd29eb48aaa8e448456aec458dc4c46292da
ozoneRatchapon <61614162+ozoneRatchapon@users.noreply.github.com>
2025-04-28 11:06:54 +0700

    Add Spider dependency with initial web crawler implementation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use spider::tokio;
+use spider::website::Website;
+
+#[tokio::main]
+async fn main() {
+    let url = "https://choosealicense.com";
+    let mut website = Website::new(&url);
+    website.crawl().await;
+
+    for link in website.get_links() {
+        println!("- {:?}", link.as_ref());
+    }
 }
